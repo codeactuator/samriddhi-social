411ecee71f34ac3774c050644d7541143ee764bb
Shekhar Kumar <shekharkumargupta@gmail.com>
2020-09-06 09:54:48 +0530

    Initial commit

---
+++ src/main/java/com/codeactuator/samriddhi/SamriddhiSocialApplication.java
@@ -0,0 +1,13 @@
+package com.codeactuator.samriddhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamriddhiSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamriddhiSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeactuator/samriddhi/SamriddhiSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeactuator.samriddhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamriddhiSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
